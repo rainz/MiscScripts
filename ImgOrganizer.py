852e30f6d530e4cefa9e81ffe036285ec1aa4190
Y Z <rainzforever@gmail.com>
2020-03-07 11:33:00 -0500

    Misc changes

---
+++ ImgOrganizer.py
@@ -0,0 +1,125 @@
+import glob
+import os
+import shutil
+
+import reverse_geocode
+
+from gmplot import gmplot
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+from PIL.ExifTags import GPSTAGS
+
+def get_if_exist(data, key):
+    if key in data:
+        return data[key]
+    return None
+def get_decimal_from_dms(dms, ref):
+
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1] / 60.0
+    seconds = dms[2][0] / dms[2][1] / 3600.0
+
+    if ref in ['S', 'W']:
+        degrees = -degrees
+        minutes = -minutes
+        seconds = -seconds
+
+    return round(degrees + minutes + seconds, 5)
+
+
+def get_coordinates(geotags):
+    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+
+    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+
+    return (lat,lon)
+
+
+def get_geodata(filename):
+    image = Image.open(filename)
+    image.verify()
+    exif_data = image._getexif()
+    if not exif_data:
+        raise ValueError("No EXIF found")
+    #print(exif_data)
+    
+    gps_key = None
+    for idx, tag in TAGS.items():
+        #print(idx, tag)
+        if tag == "GPSInfo":
+            gps_key = idx # should be 34853
+            break
+    
+    gps_data = exif_data[gps_key]
+    #print(gps_data)
+    geotagging = {}
+    for idx, tag in GPSTAGS.items():
+        #print(idx, tag)
+        if idx in gps_data:
+            geotagging[tag] = gps_data[idx]
+
+    #print(geotagging)
+    # Example
+    # {
+    #     'GPSVersionID': b'\x02\x02\x00\x00',
+    #     'GPSLatitudeRef': 'N',
+    #     'GPSLatitude': ((40, 1), (46, 1), (20268859, 1000000)),
+    #     'GPSLongitudeRef': 'W',
+    #     'GPSLongitude': ((73, 1), (35, 1), (43487548, 1000000)),
+    #     'GPSAltitudeRef': b'\x00',
+    #     'GPSAltitude': (3746, 100),
+    #     'GPSTimeStamp': ((19, 1), (52, 1), (54, 1)),
+    #     'GPSProcessingMethod': 'GPS',
+    #     'GPSDateStamp': '2018:05:05'
+    # }
+    return geotagging
+
+
+def main():
+    #src_dir = "/mnt/c/temp/BackupHuaweiMate9/Camera"
+    src_dir = "/mnt/c/temp/Test"
+    files = glob.glob(src_dir +"/*")
+    overwrite = False
+    debug_print = False
+    # dst_dir = os.getcwd()
+    dst_dir = src_dir
+
+    for f in files:
+        if os.path.isdir(f):
+            print("Skipping dir " + f)
+            continue
+        bname = os.path.basename(f)
+        parts = bname.split("_")
+        if len(parts) < 3:
+            print("Skipping file " + bname)
+            continue
+        yyyymmdd = parts[1]
+        if len(yyyymmdd) != 8:
+            print("Can't parse date in file name, skipping: " + bname)
+            continue
+        year = yyyymmdd[0:4]
+        month = yyyymmdd[4:6]
+        day = yyyymmdd[6:]
+        dst_dir_path = os.path.join(dst_dir, year, month)
+        dst_file_path = os.path.join(dst_dir_path, bname)
+        if not overwrite and os.path.exists(dst_file_path):
+            print(dst_file_path + " exists, not overwriting")
+            continue
+        if debug_print:
+            print("src: "+f)
+            print("dst: "+dst_file_path)
+        os.makedirs(dst_dir_path, exist_ok = True)
+        shutil.move(f, dst_file_path)
+
+if __name__ == "__main__":    
+    #main()
+    
+    #geo = get_geodata('/mnt/c/temp/TestCopy/IMG_20180505_155254.jpg')
+    geo = get_geodata('/mnt/c/temp/TestCopy/IMG_20191027_181134_1.jpg')
+    coord = get_coordinates(geo)
+    print(reverse_geocode.search([coord]))
+    
+    #gmap = gmplot.GoogleMapPlotter(coord[0], coord[1], 13)
+    #gmap.apikey = "AIzaSyAfD74ojnRtr4wZGLnRnr9VAQ2Lm40fQcQ"
+    #gmap.draw("my_map.html")
--- finddup.py
+++ finddup.py
@@ -10,57 +10,102 @@ def md5_file(fname, block_size=2**20):
             if not data:
                 break
             md5.update(data)
-    return md5.hexdigest()
+    res = md5.hexdigest()
+    return res
+
+
+def is_trash_dir(dname):
+    return dname.find("$RECYCLE") >= 0
+
 
 def main():
-    MIN_SIZE = 0
+    if len(sys.argv) > 1:
+        base_dir = sys.argv[1]
+        if base_dir[-1] == os.sep:
+            base_dir = base_dir[0:-1]
+    else:
+        base_dir = os.getcwd()
+    print ("Base dir:", base_dir, file=sys.stderr)
+
+    MIN_SIZE = 1024*1024
+    visit_hidden_dir = False
+    visit_hidden_file = False
+    visit_trash = False
+
+    file_count = 0
+
     size_hash = {}
+    curr_level = [base_dir]
+    while len(curr_level) > 0:
+        next_level = []
+        files = []
+        # Get a list of files and dir
+        for d in curr_level:
+            try:
+                children = os.listdir(d)
+                #print("======= {} =======".format(d))
+                for c in children:
+                    is_hidden = c.startswith(".")
+                    p = os.path.join(d, c)
+                    if os.path.isdir(p):
+                        if not visit_hidden_dir and is_hidden:
+                            continue
+                        if not visit_trash and is_trash_dir(c):
+                            continue
+                        next_level.append(p)
+                    else:
+                        if visit_hidden_file or not is_hidden:
+                            files.append(p)
+            except PermissionError:
+                continue
 
-    file_count = 0;
-
-    for root, dirs, files in os.walk("."):
-        path = root.split(os.sep)
-        #print root
-        #for d in dirs:
-        #    print "d", d
         for f in files:
             file_count += 1
             if file_count % 1000 == 0:
-                print >> sys.stderr, "{} files found".format(file_count)
-            full_path = os.path.join(root, f)
-            file_size = os.stat(full_path).st_size
-            #print "f", f, file_size
+                print("{} files found".format(file_count), file=sys.stderr)
+            file_size = os.stat(f).st_size
+            if file_size < MIN_SIZE:
+                continue
+            #print("File: ", f, file_size)
+            # Group files of the same size together
             file_set = size_hash.get(file_size)
             if file_set is None:
                 file_set = set()
                 size_hash[file_size] = file_set
-            file_set.add(full_path)
+            file_set.add(f)
 
-    print >> sys.stderr, "Found {} files. Now looking for duplicates...".format(file_count)
+        curr_level = next_level
+        continue
 
-    for size_key, file_set in size_hash.iteritems():
-        if size_key <= MIN_SIZE:
-            continue
+    print("Found {} files. Now looking for duplicates...".format(file_count), file=sys.stderr)
+
+    for size_key, file_set in size_hash.items():
+        #if size_key <= MIN_SIZE:
+        #    continue
         md5sum_hash = {}
+        if len(file_set) < 2:
+            continue # only 1 file of this size, so there's no dup and don't compute md5
         for full_path in file_set:
             try:
                 md5 = md5_file(full_path)
             except EnvironmentError:
-                print >> sys.stderr, "Unable to open {}, skipping".format(full_path)
+                print("Unable to open {}, skipping".format(full_path))
                 continue
             same_set = md5sum_hash.get(md5)
             if same_set is None:
                 same_set = set()
                 md5sum_hash[md5] = same_set
             same_set.add(full_path)
-        for md5_key, same_set in md5sum_hash.iteritems():
+        for md5_key, same_set in md5sum_hash.items():
             if len(same_set) <= 1:
                 continue
-            print "Dup files: {"
+            print("Dup files: {")
             for f in same_set:
-                print f
-            print "}"
+                print(f)
+            print("}")
 
-    print >> sys.stderr, "Done".format(file_count)
+    print("Done with {} files".format(file_count), file=sys.stderr)
 
-main()
+
+if __name__ == "__main__":    
+    main()
